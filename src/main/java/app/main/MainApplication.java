60668b9a5e57d48291868958cb8b2e4bbec0e9d4
AllysBlaze <karoprusakowska@gmail.com>
2023-12-03 10:23:52 +0100

    commitc

--- src/main/java/app/main/MainApplication.java
+++ src/main/java/app/main/MainApplication.java
@@ -3,11 +3,12 @@ package app.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.baeldung.persistence.repo")
 @EntityScan("com.baeldung.persistence.model")
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MainApplication {
 
     public static void main(String[] args) {
