2392319aeb9686542f4f4bdd0d52681748ed93de
AllysBlaze <karoprusakowska@gmail.com>
2023-12-11 17:26:45 +0100

    commitc

--- src/main/java/app/main/data/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package app.main.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-    List<Book> findByTitle(String title);
-}
